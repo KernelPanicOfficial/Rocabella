b6c69b123791ecbe4bd8073d4de63fd2c2383999
unknown <nickvourd97@gmail.com>
2024-04-03 02:26:30 +0300

    Update Arguments

--- Packages/Arguments/Arguments.go
+++ Packages/Arguments/Arguments.go
@@ -3,6 +3,7 @@ package Arguments
 import (
 	"Rocabella/Packages/Colors"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -20,11 +21,11 @@ var (
 888  T88b Y88..88P Y88b.    888  888 888 d88P Y8b.     888 888 888  888 
 888   T88b "Y88P"   "Y8888P "Y888888 88888P"   "Y8888  888 888 "Y888888 																	                                                                                       
 `
-	__version__ = "1.0"
-	//__version_name__ = "Death Star"
-	__authors__ = "@nickvourd"
-	__license__ = "MIT"
-	__github__  = "https://github/nickvourd/Rocabella"
+	__version__      = "1.0"
+	__version_name__ = "Death Star"
+	__authors__      = "@nickvourd"
+	__license__      = "MIT"
+	__github__       = "https://github/nickvourd/Rocabella"
 
 	text = `
 Rocabella v%s - Sniffing files generator.
@@ -54,22 +55,58 @@ func init() {
 	RocabellaCli.AddCommand(libArgument)
 	RocabellaCli.AddCommand(urlArgument)
 	RocabellaCli.AddCommand(lnkArgument)
+
+	// Add flags to specific commands
+	// For sc command
+	scArgument.Flags().StringP("target", "t", "", "Set target host")
+	scArgument.Flags().StringP("icon", "i", "", "Set icon display")
+	scArgument.Flags().StringP("output", "o", "", "set output file")
+
+	// For lib command
+	libArgument.Flags().StringP("target", "t", "", "Set target host")
+	libArgument.Flags().StringP("icon", "i", "", "Set icon display")
+	libArgument.Flags().StringP("output", "o", "", "set output file")
+
+	// For url command
+	urlArgument.Flags().StringP("target", "t", "", "Set target host")
+	urlArgument.Flags().StringP("icon", "i", "", "Set icon display")
+	urlArgument.Flags().StringP("output", "o", "", "set output file")
+
+	// For lnk command
+	lnkArgument.Flags().StringP("target", "t", "", "Set target host")
+	lnkArgument.Flags().StringP("icon", "i", "", "Set icon display")
+	lnkArgument.Flags().StringP("output", "o", "", "set output file")
 }
 
 // StartRocabella function
 func StartRocabella(cmd *cobra.Command, args []string) error {
+	logger := log.New(os.Stderr, "[!] ", 0)
+
 	// Call function named ShowAscii
 	ShowAscii()
+
 	// Check if additional arguments were provided.
-	if len(os.Args) <= 2 {
+	if len(os.Args) == 1 {
 		// Display help message.
 		err := cmd.Help()
+
+		// If error exists
 		if err != nil {
+			logger.Fatal(" Error:", err)
 			return err
 		}
-		// Exit the program.
-		os.Exit(0)
 	}
+
+	// Obtain version flag
+	versionFlag, err := cmd.Flags().GetBool("version")
+
+	// If error exists
+	if err != nil {
+		return err
+	}
+
+	// Call function names ShowVersion
+	ShowVersion(versionFlag)
 
 	return nil
 }
@@ -80,4 +117,14 @@ func ShowAscii() {
 	randomColor := Colors.RandomColor()
 	fmt.Print(randomColor(__ascii__))
 	fmt.Printf(text, __version__, __license__, __authors__, __github__)
+}
+
+// ShowVersion function
+func ShowVersion(versionFlag bool) {
+	// if one argument
+	if versionFlag {
+		// if version flag exists
+		fmt.Printf("[+] Current version: " + Colors.BoldRed(__version__) + "\n\n[+] Version name: " + Colors.BoldRed(__version_name__) + "\n\n")
+		os.Exit(0)
+	}
 }
