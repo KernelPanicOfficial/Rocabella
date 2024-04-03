b7668bb511bb3c9d71fa84bbd6fcf5888f640003
unknown <nickvourd97@gmail.com>
2024-04-03 03:02:46 +0300

    Update Arguments

--- Rocabella.go
+++ Rocabella.go
@@ -3,15 +3,20 @@ package main
 import (
 	"Rocabella/Packages/Arguments"
 	"Rocabella/Packages/Utils"
+	"log"
+	"os"
 )
 
 // main function
 func main() {
+	logger := log.New(os.Stderr, "[!] ", 0)
+
 	// Call function named CheckGoVersion
 	Utils.CheckGoVersion()
 
 	error := Arguments.RocabellaCli.Execute()
 	if error != nil {
+		logger.Fatal("Error", error)
 		return
 	}
 }
